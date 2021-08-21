bab104191b8957d48487a14e0610a0b1150e4b14
Prasanta Mandal <prasanta.mtechdmc@gmail.com>
2021-08-21 18:55:34 +0530

    Number of 1 Bits
    Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).

---
+++ Bit Manipulation/number of set bit.cpp
@@ -0,0 +1,33 @@
+// LeetCode: https://leetcode.com/problems/number-of-1-bits/
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        // solution-1
+        if(0) {
+            int count = 0;
+            for(int i=0; i<32; i++) {
+                if(n & (1 << i))
+                    count++;
+            }
+            return count;
+        }
+        // solution-2
+        if(0) {
+            int count = 0;
+            while(n) {
+                n = n&(n-1);
+                count++;
+            }
+            return count;
+        }
+        // solution-3
+        int count = 0;
+        while(n > 0) {
+            if(n&1)
+                count++;
+            n = n >> 1;
+        }
+        return count;
+    }
+};
